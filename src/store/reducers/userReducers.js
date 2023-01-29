10bd11ab6c3c447adcd8b7ac4629f5982b7a9ae5
mahmoud <mahmoud.shakouryan.dev@gmail.com>
2023-01-29 22:27:08 +0330

    commit

--- src/store/reducers/userReducers.js
+++ src/store/reducers/userReducers.js
@@ -1,43 +1,50 @@
 import * as actions from "../actions/actionTypes";
 
-const userSigninReducerInState = localStorage.getItem('userInfo') ? { userInfo: JSON.parse(localStorage.getItem('userInfo'))} : {};
+const userSigninReducerInState = localStorage.getItem("userInfo")
+  ? { userInfo: JSON.parse(localStorage.getItem("userInfo")) }
+  : {};
 
-export const userSigninReducer = (state = userSigninReducerInState, action ) => {
-    switch(action.type){
-        case actions.USER_SIGNIN_REQUEST:
-            return { ...state, loading: true };
-        case actions.USER_SIGNIN_SUCCESS:
-            const updatedState = { ...state, loading: false, userInfo: action.payload };
-            localStorage.setItem('userInfo', JSON.stringify(action.payload));
-            return updatedState;
-        case actions.USER_SIGNIN_FAIL:
-            return { loading: false, error: action.payload };
-        case actions.USER_SIGNOUT:
-            localStorage.removeItem('userInfo');
-            localStorage.removeItem('cardItems');
-            return {};
-        default:
-            return state;
-    }
-}
+export const userSigninReducer = (state = userSigninReducerInState, action) => {
+  switch (action.type) {
+    case actions.USER_SIGNIN_REQUEST:
+      return { ...state, loading: true };
+    case actions.USER_SIGNIN_SUCCESS:
+      const updatedState = {
+        ...state,
+        loading: false,
+        userInfo: action.payload,
+      };
+      localStorage.setItem("userInfo", JSON.stringify(action.payload));
+      return updatedState;
+    case actions.USER_SIGNIN_FAIL:
+      return { ...state, loading: false, error: action.payload };
+     case actions.USER_SIGNOUT:
+      localStorage.removeItem("userInfo");
+      return {};
+    default:
+      return state;
+  }
+};
+
 
 
 const userSignupInState = {};
 
-export const userSignupReducer = ( state = userSignupInState, action ) =>{
-    switch(action.type){
-        case actions.USER_SIGNUP_REQUEST:
-            return { ...state, loading: true };
-        case actions.USER_SIGNUP_SUCCESS:
-            const updatedSate = { ...state, loading: false, userInfo : action.payload };
-            localStorage.setItem('userInfo', JSON.stringify(action.payload));
-            return updatedSate;
-        // case actions.USER_SIGNUP_RESET:
-        //     return {};
-        case actions.USER_SIGNUP_FAIL:
-            return { loading: false, error: action.payload };
-        default:
-            return state;
-
-    }
-}
+export const userSignupReducer = (state = userSignupInState, action) => {
+  switch (action.type) {
+    case actions.USER_SIGNUP_REQUEST:
+      return { ...state, loading: true };
+    case actions.USER_SIGNUP_SUCCESS:
+      const updatedSate = {
+        ...state,
+        loading: false,
+        userInfo: action.payload,
+      };
+      localStorage.setItem("userInfo", JSON.stringify(action.payload));
+      return updatedSate;
+    case actions.USER_SIGNUP_FAIL:
+      return { loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
